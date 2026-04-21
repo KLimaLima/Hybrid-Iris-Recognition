ef456ac787a544bf5f31f2964daaf150195c577f
Khairul Ikhwan <khairul0355@hotmail.com>
2026-04-21 22:15:51 +0800

    refactor def show

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import fractal
 import cv2
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import json
 import time
 
@@ -15,18 +16,25 @@ def main():
 
     df = pd.read_csv("db/CASIA-Iris-Thousand.csv")
 
+    # for path_to_image in df['NewPath']:
+
+    #     path_to_process = f'{PATH_DB}{path_to_image}'
+    #     print(f'Processing {path_to_process}')
+
     selected_path = df['NewPath'].loc[df.index[11070]] # 11070
 
     selected_path = f'{PATH_DB}{selected_path}'
-
-    # show_iris_code(df)
+    
+    show(selected_path)
 
     # get_data_np(selected_path)
 
-    npz_path = f'{selected_path.split('.')[0]}.npz'
-    npz_file = np.load(npz_path, allow_pickle=True)
-    my_dict = npz_file['fractal_dimension'].item()
-    print(my_dict['box_counting_original'])
+    # npz_path = f'{selected_path.split('.')[0]}.npz'
+    # npz_file = np.load(npz_path, allow_pickle=True)
+    # cv2.imshow('image from npz file', npz_file['img_original'])
+    # cv2.waitKey(100)
+    # my_dict = npz_file['fractal_dimension'].item()
+    # print(type(my_dict['box_counting_original']['boxes'])) # numpy array
 
 def get_data_np(img_path: str):
 
@@ -66,53 +74,57 @@ def get_coords(dataframe):
 
         inference.get_coords_curl(path_to_process)
 
-def show_iris_code(dataframe):
-    dataframe = pd.read_csv("db/CASIA-Iris-Thousand.csv")
+def show(path_img: str):
 
-    selected_path = dataframe['NewPath'].loc[dataframe.index[11070]] # 11070
-
-    selected_path = f'{PATH_DB}{selected_path}'
-    split_xtension = selected_path.split('.')
+    split_xtension = path_img.split('.')
     json_path = f'{split_xtension[0]}.json'
 
-    img  = cv2.imread(selected_path, cv2.IMREAD_GRAYSCALE)
-
-    print(img.shape)
+    img  = cv2.imread(path_img, cv2.IMREAD_GRAYSCALE)
 
     with open(json_path, "r") as j:
         data_json = json.load(j)
 
-    # print(data_json['predictions'][0]['class'])
-
-    # NOTE: to use show_details, must cv2.imread(img_path) - cannot use grayscale
-    # iris.show_details(img,
-    #                 data_json['predictions'][0]['x'],
-    #                 data_json['predictions'][0]['y'],
-    #                 data_json['predictions'][0]['width'] / 2,
-    #                 data_json['predictions'][1]['x'],
-    #                 data_json['predictions'][1]['y'],
-    #                 data_json['predictions'][1]['width'] / 2)
-
-    points_iris = iris.unravel_iris(img,
-                    data_json['predictions'][0]['x'],
-                    data_json['predictions'][0]['y'],
-                    data_json['predictions'][0]['width'] / 2,
-                    data_json['predictions'][1]['x'],
-                    data_json['predictions'][1]['y'],
-                    data_json['predictions'][1]['width'] / 2)
+    pupil_data = {'x': data_json['predictions'][0]['x'],
+                  'y': data_json['predictions'][0]['y'],
+                  'r': data_json['predictions'][0]['width'] / 2}
     
-    print(points_iris.shape)
+    limbus_data = {'x': data_json['predictions'][1]['x'],
+                  'y': data_json['predictions'][1]['y'],
+                  'r': data_json['predictions'][1]['width'] / 2}
 
-    calc_fractal(points_iris, True)
+    code_iris = iris.unravel_iris(img,
+                                  limbus_data['x'], limbus_data['y'], limbus_data['r'],
+                                  pupil_data['x'], pupil_data['y'], pupil_data['r'])
+    
+    calc_fractal(code_iris, True)
 
-    # # Define scales
-    # scales = np.logspace(0, 10, num=50)
+    my_hist_eq = cv2.equalizeHist(img)
 
-    # # Compute fractal dimension
-    # result = fractal.box_counting(points_iris, scales, method="original")
+    code_iris_histeq = iris.unravel_iris(my_hist_eq,
+                                  limbus_data['x'], limbus_data['y'], limbus_data['r'],
+                                  pupil_data['x'], pupil_data['y'], pupil_data['r'])
+    
+    calc_fractal(code_iris_histeq, True)
 
-    # print("Fractal Dimension:", result["fd"])
-    # print("R²:", result["r_squared"])
+    print(limbus_data)
+
+    cv2.circle(img, (int(limbus_data['x']), int(limbus_data['y'])), int(limbus_data['r']), (255, 0, 0), 3)
+    cv2.circle(img, (int(limbus_data['x']), int(limbus_data['y'])), 2, (255, 0, 0), 2)
+    cv2.circle(img, (int(pupil_data['x']), int(pupil_data['y'])), int(pupil_data['r']), (0, 255, 0), 3)
+    cv2.circle(img, (int(pupil_data['x']), int(pupil_data['y'])), 2, (0, 255, 0), 2)
+
+    f, axes = plt.subplots(2, 2, figsize=(8, 8))
+    axes[0, 0].imshow(img, cmap=plt.cm.gray)
+    axes[0, 0].set_title('Original Iris')
+    axes[0, 1].imshow(code_iris, cmap=plt.cm.gray)
+    axes[0, 1].set_title('Original Code')
+
+    axes[1, 0].imshow(my_hist_eq, cmap=plt.cm.gray)
+    axes[1, 0].set_title('Hist EQ Iris')
+    axes[1, 1].imshow(code_iris_histeq, cmap=plt.cm.gray)
+    axes[1, 1].set_title('Hist EQ Code')
+
+    plt.show()
 
 def calc_fractal(code_iris, show= False):
 
