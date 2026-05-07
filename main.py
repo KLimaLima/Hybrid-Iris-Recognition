aa60f4dcc480498d8aa5c703b5062a61d34bbf6d
Khairul Ikhwan <khairul0355@hotmail.com>
2026-05-08 00:12:32 +0800

    forgot to edit prepare

--- main.py
+++ main.py
@@ -175,9 +175,10 @@ def prepare_input_gcolab(list_path):
         dict_gabor_phase = npz_file['gabor_phase'].item()
         gabor_phase_list = list(dict_gabor_phase.values())
 
-        temp1 = np.expand_dims(gabor_phase_list[0], axis=2)
+        # temp1 = np.expand_dims(gabor_phase_list[0], axis=2)
         # cv2.cvtColor(temp1, cv2.COLOR_GRAY2BGR)
-        X_gabor1.append(temp1)
+        # X_gabor1.append(temp1)
+        X_gabor1.append(npz_file['code_hist_eq'])
 
         temp2 = np.expand_dims(gabor_phase_list[1], axis=2)
         # cv2.cvtColor(temp2, cv2.COLOR_GRAY2BGR)
